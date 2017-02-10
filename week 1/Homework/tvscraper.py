ee1b1f6df3eaccc9f27a4f414f3ef6dc4cd7a8c8
alexwit <wittebroodalex@gmail.com>
2017-02-10 13:07:38 +0100

    Homework week 1

--- week 1/Homework/tvscraper.py
+++ week 1/Homework/tvscraper.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Alex Wittebrood
+# Student number: 10288880
 '''
 This script scrapes IMDB and outputs a CSV file with highest rated tv series.
 '''
 import csv
-from bs4 import BeautifulSoup
-from pattern.web import URL, DOM, download
+#from bs4 import BeautifulSoup
+from pattern.web import URL, DOM, plaintext
 
-soup = BeautifulSoup(BACKUP_HTML, 'html.parser')
+
 TARGET_URL = "http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&start=1&title_type=tv_series"
 BACKUP_HTML = 'tvseries.html'
 OUTPUT_CSV = 'tvseries.csv'
@@ -18,9 +18,6 @@ def extract_tvseries(dom):
     '''
     Extract a list of highest rated TV series from DOM (of IMDB page).
     
-    
-
-
     Each TV series entry should contain the following fields:
     - TV Title
     - Rating
@@ -33,14 +30,36 @@ def extract_tvseries(dom):
     # HIGHEST RATED TV-SERIES
     # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
     # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
+    
+    list_highest_rated_series = []
+    
+    for i in range(0,50):
+             
+        # extracts title of the series and saves it to the string
+        tv_title = dom("h3.lister-item-header")[i].by_tag('a')[0].content.encode("latin-1")
+        serie_detail = (tv_title + ", ")
 
-    for e in dom('div.lister-item-content')[:3]:
-        print("firstloop")
-        for a in e('h3.lister-item-header')[:1]:
-            print(soup.find_all('a').get_text())
+        # extracts genre of the serie and saves it to the string
+        genre = dom.by_class("genre")[i].content[1:].rstrip().encode("latin-1")
+        serie_detail += (genre + ", ") 
+        
+        # extracts runtime of an episode and saves it to the string
+        runtime = dom.by_class("runtime")[i+1].content[:-3].encode("latin-1")
+        serie_detail += (runtime + ", ")
+        
+        # extracts the 4 lead actors of the series and saves it to the string
+        actors = ""
+        for j in range(0,4):
+            actors += dom.by_class("lister-item-content")[i].by_tag("p")[2].by_tag("a")[j].content.encode("latin-1") + ","
+        serie_detail += actors
 
-
-    return []  # replace this line as well as appropriate
+        # extracts the rating of the series and saves it to the string
+        rating = plaintext(dom.by_class("inline-block ratings-imdb-rating")[i].content)
+        serie_detail += (rating.encode("latin-1"))
+       
+       
+        list_highest_rated_series.append(serie_detail)
+    return list_highest_rated_series  # replace this line as well as appropriate
 
 
 def save_csv(f, tvseries):
@@ -49,6 +68,7 @@ def save_csv(f, tvseries):
     '''
     writer = csv.writer(f)
     writer.writerow(['Title', 'Rating', 'Genre', 'Actors', 'Runtime'])
+    writer.writerows([x.split(',') for x in tvseries])
 
     # ADD SOME CODE OF YOURSELF HERE TO WRITE THE TV-SERIES TO DISK
 
@@ -66,9 +86,9 @@ if __name__ == '__main__':
     dom = DOM(html)
 
     # Extract the tv series (using the function you implemented)
-    print("tvseries")
+   
     tvseries = extract_tvseries(dom)
-
+    
     # Write the CSV file to disk (including a header)
     with open(OUTPUT_CSV, 'wb') as output_file:
         save_csv(output_file, tvseries)
